a897834e463ceca8927aacf3a215139bce39c157
pete3249 <katelynrpeterson16@gmail.com>
2020-09-05 10:32:04 -0400

    Add description to each route in users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,11 @@
 class UsersController < ApplicationController
 
-    #render form to create new user account
+    # render form to create new user account
     get '/users/new' do
         erb :'/users/new'
     end
 
-    #create new user
+    # create new user
     post '/users' do
         user = User.new(params[:user])
         if user.save
