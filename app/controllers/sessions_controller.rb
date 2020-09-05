e38f1bcbcb754a3d3ab33f359a3380f7c29b5325
pete3249 <katelynrpeterson16@gmail.com>
2020-09-05 10:22:26 -0400

    Create post '/login' route

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,14 @@ class SessionsController < ApplicationController
 
     # read information from the form and determine if they can be logged on
     post '/login' do
-        
+        user = User.find_by_email(params[:email])
+        if user && user.authenticate(params[:password])
+            session[:id] = user.id
+            redirect '/'
+        else 
+            @error = "Incorrect email or password, please try again"
+            erb :'/sessions/login'
+        end 
     end
 
 end 
