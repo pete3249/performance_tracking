1cd33bb8c5ea3d53147740eb3d0b2af636749ea2
pete3249 <katelynrpeterson16@gmail.com>
2020-09-05 13:38:07 -0400

    Add validations for Student

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,4 +1,7 @@
 class Student < ActiveRecord::Base
     belongs_to :user
     has_many :performance_tests
+    validates :first_name, presence: true
+    validates :last_name, presence: true
+    validates :grad_year, presence: true
 end
