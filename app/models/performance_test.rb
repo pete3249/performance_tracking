6d38f560e815a1e7606b50c52b028786e149271a
pete3249 <katelynrpeterson16@gmail.com>
2020-09-06 13:07:10 -0400

    Add validations for PerformanceTest model

--- app/models/performance_test.rb
+++ app/models/performance_test.rb
@@ -1,4 +1,7 @@
 class PerformanceTest < ActiveRecord::Base
     belongs_to :user
     belongs_to :student
+    validates :name, presence: true
+    validates :result, presence: true
+    validates :testing_date, presence: true
 end
