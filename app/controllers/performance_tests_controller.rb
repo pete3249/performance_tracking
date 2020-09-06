88ef91989bc8dcbb8a3a4583f3924808560b6b88
pete3249 <katelynrpeterson16@gmail.com>
2020-09-06 13:03:27 -0400

    Add index, new, and create routes

--- app/controllers/performance_tests_controller.rb
+++ app/controllers/performance_tests_controller.rb
@@ -1,28 +1,37 @@
 class PerformanceTestsController < ApplicationController
 
-  # GET: /performance_tests
   get "/performance_tests" do
-    erb :"/performance_tests/index.html"
+    @students = Student.all.sort
+    erb :"/performance_tests/index"
   end
 
-  # GET: /performance_tests/new
   get "/performance_tests/new" do
-    erb :"/performance_tests/new.html"
+    redirect_if_not_logged_in
+    find_student
+    @performance_test = PerformanceTest.new
+    erb :"/performance_tests/new"
   end
 
-  # POST: /performance_tests
   post "/performance_tests" do
-    redirect "/performance_tests"
+    find_student
+    @performance_test = @student.performance_tests.build(params[:performance_test])
+    @performance_test.user = current_user
+    if @performance_test.save
+      flash[:success] = "Test successfully stored"
+      redirect "/performance_tests"
+    else
+      erb :"/performance_tests/new"
+    end
   end
 
   # GET: /performance_tests/5
   get "/performance_tests/:id" do
-    erb :"/performance_tests/show.html"
+    erb :"/performance_tests/show"
   end
 
   # GET: /performance_tests/5/edit
   get "/performance_tests/:id/edit" do
-    erb :"/performance_tests/edit.html"
+    erb :"/performance_tests/edit"
   end
 
   # PATCH: /performance_tests/5
@@ -34,4 +43,15 @@ class PerformanceTestsController < ApplicationController
   delete "/performance_tests/:id" do
     redirect "/performance_tests"
   end
+
+  private 
+
+  def find_student
+    @student = Student.find_by_id(params[:id])
+    if @student.nil?
+      flash[:error] = "Couldn't find a student with id: #{params[:id]}"
+      redirect "/students"
+    end 
+  end 
+
 end
