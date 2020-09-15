7e01a2c2747023ed709d06b9c05f25923da521bd
pete3249 <katelynrpeterson16@gmail.com>
2020-09-15 11:00:55 -0400

    Add error message for creating new user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,18 @@
 class UsersController < ApplicationController
 
     get '/users/new' do
+        @user = User.new
         erb :'/users/new'
     end
 
     post '/users' do
-        user = User.new(params[:user])
-        if user.save
-            session[:id] = user.id
+        @user = User.new(params[:user])
+        if @user.save
+            session[:id] = @user.id
             redirect "/students"
         else
-            redirect "/users/new"
+            @params = params[:user]
+            erb :'/users/new'
         end 
     end
 
